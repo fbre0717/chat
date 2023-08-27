3205f40410b9458ea4aeee81dde890abf471c00a
fbre0717 <koh2040@naver.com>
2023-08-28 04:24:54 +0900

    feat: 서버 첫 페이지 react index

--- back/app.js
+++ back/app.js
@@ -2,10 +2,14 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require("body-parser");
 var logger = require('morgan');
+// require('dotenv').config()
 
+// const routes = require('./routes/routes');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
 
 var app = express();
 
@@ -17,10 +21,21 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// React index.html path
+app.use(express.static(path.join(__dirname, "../front/build")));
+
+// route path
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+// connect react
+// app.use(routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
